dcdf43d5c6a8a3a41d6844c4a3ce1df4c1d6316e
Vincent Galloy <vgalloy@excilys.com>
2016-02-22 08:51:37 +0100

    [ FEAT     ] : Spring jdkproxy

---
+++ src/main/java/com/vgalloy/spring/jdkproxy/service/MyService.java
@@ -0,0 +1,9 @@
+package com.vgalloy.spring.jdkproxy.service;
+
+/**
+ * @author Vincent Galloy
+ *         Created by Vincent Galloy on 19/02/16.
+ */
+public interface MyService {
+    int add(int a, int b);
+}
---
+++ src/main/java/com/vgalloy/spring/jdkproxy/service/impl/MyServiceImpl.java
@@ -0,0 +1,14 @@
+package com.vgalloy.spring.jdkproxy.service.impl;
+
+import com.vgalloy.spring.jdkproxy.service.MyService;
+
+/**
+ * @author Vincent Galloy
+ *         Created by Vincent Galloy on 19/02/16.
+ */
+public class MyServiceImpl implements MyService {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
